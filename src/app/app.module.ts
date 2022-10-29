6063162e0c05557f6d519997c0330d0f84c083c8
Vini Bartoli <vinibartoli04@gmail.com>
2022-10-29 15:29:47 -0300

    criacao component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NavbarComponent } from './navbar/navbar.component';
+import { PrimeNGModule } from './primeng.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    NavbarComponent,
-  ],
+      NavbarComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    PrimeNGModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
