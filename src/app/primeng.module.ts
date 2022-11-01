cb311ac8b0268b2f9ad404e9c1651cc3d82c473a
Vini Bartoli <vinibartoli04@gmail.com>
2022-10-31 22:33:55 -0300

    Componente principal atualizado

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from "@angular/core";
+import {InputNumberModule} from 'primeng/inputnumber';
+import {InputMaskModule} from 'primeng/inputmask';
+import {ButtonModule} from 'primeng/button';
+import {FieldsetModule} from 'primeng/fieldset';
+import {TableModule} from 'primeng/table';
 
 
 @NgModule({
     exports: [
-        
+        InputNumberModule,
+        InputMaskModule,
+        ButtonModule,
+        FieldsetModule,
+        TableModule,
     ]
 })
 
