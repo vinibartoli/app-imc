526890c479148314838403272461dba31ad5230c
Vini Bartoli <vinibartoli04@gmail.com>
2022-11-01 01:00:25 -0300

    criacao table resultado

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
+  peso: number | undefined;
+  altura: number | undefined;
+  
   constructor() { }
 
   ngOnInit() {
