2b98b33fb4704d951bca0c344409c5a8fd6b2d64
Vini Bartoli <vinibartoli04@gmail.com>
2022-11-02 01:59:14 -0300

    atualizacao inputs

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Medidas {
+  altura: number;
+  peso: number;
+}
+
+interface Resultado {
+  resultadoImc: number;
+  classificacaoImc: string;
+  obesidade: string;
+}
+
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
+
 export class PrincipalComponent implements OnInit {
 
-  peso: number | undefined;
-  altura: number | undefined;
-  
   constructor() { }
 
   ngOnInit() {
   }
-
 }
